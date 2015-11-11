baa82004cd3b8ea3f341bf30fa7c7a3ec29c6ca2
Dain Nilsson <dain@yubico.com>
2015-11-11 14:24:44 +0100

    This was broken (value doesn't have to be string).
    I'm also not sure where (if anywhere) this was used.

--- yubicommon/qt/settings.py
+++ yubicommon/qt/settings.py
@@ -41,7 +41,7 @@ def convert_to(value, target_type):
         return float(value)
     if target_type is bool:
         return value not in ['', 'false', 'False']
-    return value.encode('utf8')
+    return value
 
 
 class SettingsGroup(object):
