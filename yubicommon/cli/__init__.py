f015c535a87a48b047555144f8d0e57ace5c1907
Dain Nilsson <dain@yubico.com>
2016-01-22 15:19:59 +0100

    Added cli module.

---
+++ yubicommon/cli/__init__.py
@@ -0,0 +1,96 @@
+# Copyright (c) 2016 Yubico AB
+# All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or
+#   without modification, are permitted provided that the following
+#   conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#    2. Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+from docopt import docopt, DocoptExit
+import sys
+
+__all__ = ['CliCommand', 'Argument']
+
+
+# From PEP 0257 -- Docstring Conventions (Public Domain)
+def trim(docstring):
+    """Corrects indentation for docstrings"""
+    if not docstring:
+        return ''
+    # Convert tabs to spaces (following the normal Python rules)
+    # and split into a list of lines:
+    lines = docstring.expandtabs().splitlines()
+    # Determine minimum indentation (first line doesn't count):
+    indent = sys.maxint
+    for line in lines[1:]:
+        stripped = line.lstrip()
+        if stripped:
+            indent = min(indent, len(line) - len(stripped))
+    # Remove indentation (first line is special):
+    trimmed = [lines[0].strip()]
+    if indent < sys.maxint:
+        for line in lines[1:]:
+            trimmed.append(line[indent:].rstrip())
+    # Strip off trailing and leading blank lines:
+    while trimmed and not trimmed[-1]:
+        trimmed.pop()
+    while trimmed and not trimmed[0]:
+        trimmed.pop(0)
+    # Return a single string:
+    return '\n'.join(trimmed)
+
+
+class Argument(object):
+    def __init__(self, name, santitize=lambda x: x, default=None):
+        self._name = name
+        self._sanitize = santitize
+        self._default = default
+
+    def __call__(self, args):
+        if isinstance(self._name, basestring):
+            value = args[self._name]
+        else:
+            value = None
+            for key in self._name:
+                if args.get(key, False) is True:
+                    value = key
+                    break
+        return self._sanitize(value) if value is not None else self._default
+
+
+class CliCommand(object):
+    """Subclasses should contain a docopt compatible docstring."""
+
+    def __init__(self, *args, **kwargs):
+        # Parse args
+        self._args = docopt(trim(self.__doc__), *args, **kwargs)
+
+        # Populate Arguments from args
+        for f in dir(self):
+            value = getattr(self, f)
+            if isinstance(value, Argument):
+                try:
+                    setattr(self, f, value(self._args))
+                except ValueError as e:
+                    print "Error for option {}: {}".format(value._name, e)
+                    raise DocoptExit()
